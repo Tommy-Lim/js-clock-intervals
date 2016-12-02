d8c37bca78b966ab9a16ebaab2c441749a121244
Tommy-Lim <tlim24@gmail.com>
2016-12-01 17:55:23 -0800

    all hands rotate properly

---
+++ clock.js
@@ -0,0 +1,11 @@
+var i=0;
+var interval;
+
+var step = function(){
+  i+=1; //use 100 instead of 1 for faster animation
+  document.getElementById('second').style.transform="rotate("+(i/60*360)+"deg)";
+  document.getElementById('minute').style.transform="rotate("+(i/(60*60)*360)+"deg)";
+  document.getElementById('hour').style.transform="rotate("+(i/(60*60*12)*360)+"deg)";
+};
+
+interval = setInterval(step,1000);
